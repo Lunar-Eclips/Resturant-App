2ce51d121921d9da9bff8f09a99b9bcc7ad90ffc
mohima-moon <mohima.moon@g.bracu.ac.bd>
2025-10-02 13:32:39 +0000

    Main

---
+++ App.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class App{
+
+    public static void main(String [] args){
+        
+        Scanner sc=new Scanner(System.in);
+
+        FoodTemplate food=new FoodTemplate();
+
+     }
+
+     
+}
