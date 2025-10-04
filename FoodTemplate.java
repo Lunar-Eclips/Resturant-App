3c8e2b23fad7818db68653f568ce3582b3da5e0e
mohima-moon <mohima.moon@g.bracu.ac.bd>
2025-10-04 16:26:39 +0000

    Main

--- FoodTemplate.java
+++ FoodTemplate.java
@@ -2,6 +2,7 @@ public class FoodTemplate
 {
     public String name;
     public int price;
+    public int quantity;
     public int estimatedTime; // in seconds
 
     // for printing the object
@@ -9,6 +10,34 @@ public class FoodTemplate
     {
         System.out.println("Name: " + food.name);
         System.out.println("Price: " + food.price);
+        System.out.println("Quantity: " + food.quantity);
         System.out.println("Estimated Time: " + food.estimatedTime + " seconds");
     }
+
+    //for the bill
+    public static int Bill(int price,int quantity){
+      int bill=price*quantity;
+      return bill;
+    }
+    
+   //for discount
+    public static double DiscountAmount(double SubTotal,double Discount){
+      
+     double amount=SubTotal-Discount;
+     return amount;
+    }
+    
+    public static double DiscountPercentage(double SubTotal,double Discount){
+      
+     double amount=SubTotal-(SubTotal*Discount)/100;
+     return amount;
+    }
+    
+    //for the VAT
+    public static double VAT(double bill){
+      double Bill_after=bill+bill*.15;
+      return Bill_after;
+    }
+
+
 }
