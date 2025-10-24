785f4f84a2d0a63a805c1ea8631c3c48b423af89
Lunar-Eclips <111911330+Lunar-Eclips@users.noreply.github.com>
2025-10-24 12:53:21 +0600

    Add menu and search features to App, foodAdder to FoodTemplate
    Introduced menu selection and search functionality in App.java, allowing users to choose food categories and search for inventory, analytics, or item adder. Added the foodAdder method to FoodTemplate.java for easier food item creation. Minor formatting improvements were also made.

--- App.java
+++ App.java
@@ -13,10 +13,10 @@ public class App{
         while(true){
           
         System.out.println("Enter food name");
-        food.name=sc.nextLine();
+        food.name = sc.nextLine();
         
         if (food.name.equalsIgnoreCase("Enter")) {
-                break;//to stop order;
+                break; //to stop order;
             }
         
         System.out.println("Enter price");
@@ -56,20 +56,119 @@ public class App{
            Amount=FoodTemplate.DiscountAmount(SubTotal,(double)DiscountAmount);
         }
            else{
-           System.out.println("Enter discount  percentage:");
+           System.out.println("Enter discount  percentage: ");
            int DiscountPercentage=sc.nextInt();
            Amount=FoodTemplate.DiscountPercentage(SubTotal,(double)DiscountPercentage);
            }
         }
-      
-        
+
         
        //calculate VAT
-        double AmountAfterVAT=FoodTemplate.VAT(Amount);
+        double AmountAfterVAT=FoodTemplate.VAT( Amount );
         
         
        System.out.println("Total Amount:"+AmountAfterVAT);
 
        sc.close();
     }
+    
+    
+    // For searching
+      public static void Search(String a) {
+        
+    // for Item adder
+      if (a.trim().equalsIgnoreCase("item adder")) {
+         System.out.println("Item Adder");
+//         itemAdder();
+    }
+      
+      // for searching inventory
+      if (a.trim().equalsIgnoreCase("Inventory")){
+        System.out.println("Inventory");
+//       inventory();
+      }
+      
+      // for searching Analytics
+      if (a.trim().equalsIgnoreCase("Analytics")){
+      System.out.println("Analytics");
+//      Analytics();
+      
+      }
+      
+}
+     
+      public void menu(){
+        
+      Scanner sc = new Scanner( System.in );
+      System.out.println("Choose Catogory");
+      System.out.println("Main Course");
+      System.out.println("Side Dish");
+      System.out.println("Dessert");
+      System.out.println("Beverage");
+      
+      String input = sc.nextLine();
+      
+      
+      if ( input.equalsIgnoreCase("Main course")){
+         mainCourse();
+       }
+      
+      else if ( input.equalsIgnoreCase("side dish")){
+         sideDish();
+       }
+      
+      else if ( input.equalsIgnoreCase("Dessert")){
+         dessert();
+       }
+      
+      else if ( input.equalsIgnoreCase("Bevarage")){
+         bevarage();
+       }
+      else {
+      System.out.println("Wrong Input");
+      }
+      
+      
+      }
+      
+      public void mainCourse(){
+      FoodTemplate friedRice = new FoodTemplate();
+      friedRice.foodAdder("Fried rice", 200, 300);
+      FoodTemplate.details(friedRice);
+      
+      FoodTemplate palou = new FoodTemplate();
+      palou.foodAdder("Palou", 200, 300);
+      FoodTemplate.details(palou);
+      }
+      
+      public void sideDish(){
+      FoodTemplate frenchFries = new FoodTemplate();
+      frenchFries.foodAdder("French Fries", 100, 60);
+      FoodTemplate.details(frenchFries);
+      
+      FoodTemplate pasta = new FoodTemplate();
+      pasta.foodAdder("Pasta", 150, 300);
+      FoodTemplate.details(pasta);
+      }
+      
+      public void dessert(){
+      FoodTemplate mishti = new FoodTemplate();
+      mishti.foodAdder("Mishti", 300, 100);
+      FoodTemplate.details(mishti);
+      
+      FoodTemplate cake = new FoodTemplate();
+      cake.foodAdder("Cake", 600, 300);
+      FoodTemplate.details(cake);
+      }
+      
+      public void bevarage(){
+      FoodTemplate fanta = new FoodTemplate();
+      fanta.foodAdder("Fanta", 20, 10);
+      FoodTemplate.details(fanta);
+      
+      FoodTemplate lemu = new FoodTemplate();
+      lemu.foodAdder("Lemu", 20, 10);
+      FoodTemplate.details(lemu);
+      }
+     
  }
